740eebd121ed437091debd59997cb4b93d5fd0ea
Michael Matheis <github@michbert.de>
2015-05-31 14:27:17 +0200

    additional api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
     end
     delete '/', to: 'hangar/records#delete'
   end
+
+  #TODO: make configurable
+  namespace :api
+    constraints Hangar::RouteConstraint.new do
+      FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
+        resources factory, only: [:new, :create], controller: 'hangar/resources'
+      end
+      delete '/', to: 'hangar/records#delete'
+    end
+  end
 end
