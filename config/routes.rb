d64351552625fa7ca746de0fadb732f0d382ed1e
Michael Matheis <github@michbert.de>
2015-05-31 15:06:02 +0200

    remove root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  constraints Hangar::RouteConstraint.new do
-    FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
-      resources factory, only: [:new, :create], controller: 'hangar/resources'
-    end
-    delete '/', to: 'hangar/records#delete'
-  end
-
   #TODO: make configurable
   scope(:path => '/api') do
     constraints Hangar::RouteConstraint.new do
