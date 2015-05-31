175b84fef52feccd19898ed107a3074e828cdbab
Michael Matheis <github@michbert.de>
2015-05-31 15:11:25 +0200

    fix scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   #TODO: make configurable
-  scope(:path => '/api') do
-    constraints Hangar::RouteConstraint.new do
-      FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
+  
+  constraints Hangar::RouteConstraint.new do
+    FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
+      scope(:path => '/api') do
         resources factory, only: [:new, :create], controller: 'hangar/resources'
       end
-      delete '/', to: 'hangar/records#delete'
     end
+    delete '/api/', to: 'hangar/records#delete'
   end
+
 end
